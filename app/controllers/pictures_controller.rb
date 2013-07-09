e67396713e9e32410232ae6b21b7746d436d7eb7
Anish Khithani <anish.khithani@gmail.com>
2013-07-09 12:23:29 -0400

    configuring root as pictures#index

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,11 +8,19 @@ class PicturesController < ApplicationController
   end
 
   def new
-
+    @picture = Picture.new
   end
 
   def create
-    render :text =>"Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+    @picture = Picture.new(params[:picture])
+    if @picture.save
+      #if the save is successful, go to index.html.erb mainpage
+      redirect_to pictures_path
+    else
+      #otherwise show the new form again so they can fill it out properly
+      render pictures_new_path
+    end
+
   end
 
 
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,6 @@
 class Picture < ActiveRecord::Base
   attr_accessible :artist, :title, :url
+  validates :artist, :presence => true
+  validates :title, :presence => true
+  validates :url, :presence => true
 end
